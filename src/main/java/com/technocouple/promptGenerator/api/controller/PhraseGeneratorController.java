7a50294545c021f3f4c76c898db122acc43f5710
shivaprasadmohanrao <79432422+shivaprasadmohanrao@users.noreply.github.com>
2023-02-26 18:21:23 +0530

    updating Controller

--- src/main/java/com/technocouple/promptGenerator/api/controller/PhraseGeneratorController.java
+++ src/main/java/com/technocouple/promptGenerator/api/controller/PhraseGeneratorController.java
@@ -1,26 +1,13 @@
 package com.technocouple.promptGenerator.api.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.technocouple.promptGenerator.api.dao.CreateIntentRequest;
-import com.technocouple.promptGenerator.api.dao.OpenAITextCompletion;
-import com.technocouple.promptGenerator.api.dao.OpenAITextCompletionReq;
-import com.technocouple.promptGenerator.api.dao.Parts;
-import com.technocouple.promptGenerator.api.dao.TrainingPhrases;
 import com.technocouple.promptGenerator.api.dao.UserInput;
-import com.technocouple.promptGenerator.api.dao.DFCXApiResponse;
+import com.technocouple.promptGenerator.api.repository.DialogflowCXIntegration;
+import com.technocouple.promptGenerator.api.repository.OpenAIIntegration;
 
 //PreRequisities:
 //1.OpenAI account, API key
@@ -45,7 +32,7 @@ public class PhraseGeneratorController {
 		System.out.println("User entered intent : " + messages.getInput());
 		intentName = messages.getInput();
 		//call to OpenAI Completions API
-		String[] entries = openAIPromptGenerator(messages.getInput()).split("\n");
+		String[] entries = OpenAIIntegration.openAIPromptGenerator(messages.getInput()).split("\n");
 		int noOfPhrase = entries.length;
 		StringBuilder entryText = new StringBuilder();
 		//Ensuring the Bullets and numberings are removed from the OpenAI response and add newline feed.
@@ -78,142 +65,8 @@ public class PhraseGeneratorController {
 		//please use log lib as required
 		System.out.println("User entered intent : " + messages.getInput());
 		//System.out.println("DFCX Phrases  : " + openAIPhrases);
-		createDfcxVirtualAgent(openAIPhrases);
+		DialogflowCXIntegration.createDfcxVirtualAgent(openAIPhrases,intentName);
 		return "Sucessfully Updated into your Dialogflow CX Console!";
 	}
-	/**
-	 * This method connects to OpenAI completions API and return the Phrases
-	 * @param intent
-	 * @return
-	 */
-	private static String openAIPromptGenerator(String intent) {
-		//OpenAI URL : <base_url> + <completions_api>
-		String url = "https://api.openai.com/v1/completions";
-		//add Headers
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		List<MediaType> list = new ArrayList<MediaType>();
-		list.add(MediaType.ALL);
-		headers.setAccept(list);
-		//get this from https://beta.openai.com/account/api-keys 90days validity for free account
-		headers.setBearerAuth("<OpenAI_API_KEY>");
-		
-		//Build Request object for OpenAI Completion API
-		OpenAITextCompletionReq mRequest = new OpenAITextCompletionReq();
-		//set Model as TEXT_DAVINCI
-		mRequest.setModel("text-davinci-003");
-		//PromptEngineering - prompt to generate training phrases for a given Intent
-		mRequest.setPrompt("Generate 10 professional user utterances for the intent "
-				+ "\""+ intent +"\" and display without numbering and bullet points");
-		//Temperature helps to create phrases in creative to realistic way based on the configured value
-		mRequest.setTemperature(0.2);
-		mRequest.setMax_tokens(450);
-		mRequest.setTop_p(1);
-		mRequest.setFrequency_penalty(0);
-		mRequest.setPresence_penalty(0);
-		
-		HttpEntity<OpenAITextCompletionReq> waRequest = new HttpEntity<OpenAITextCompletionReq>(mRequest, headers);
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<OpenAITextCompletion> response = restTemplate.exchange(url, HttpMethod.POST, waRequest, OpenAITextCompletion.class);
-		return response.getBody().getChoices()[0].getText();
-	}
-    /**
-     * This method helps in invoking Google Dialogflow CX api's to 
-     * 1. Create Intent and upload Training Phrases
-     * 2. Train the Virtual Agent
-     * @param trainingPhrases
-     */
-    private static void createDfcxVirtualAgent(String[] trainingPhrases) {
-    	int numOfPhrase = trainingPhrases.length;
-    	String entryText = "";
-    	System.out.println("NumberOfPhrases : " + numOfPhrase);
-    	
-    	//URL of Dialogflow CX :
-    	//POST https://REGION_ID-dialogflow.googleapis.com/v3/projects/PROJECT_ID/locations/REGION_ID/agents/AGENT_ID/intents
-    	String url = "https://global-dialogflow.googleapis.com/v3/projects/<<PROJECT_ID>>/locations/"
-    			+ "<<REGION_ID>>/agents/<<AGENT_ID>>/intents";
-    	//add headers
-    	HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		List<MediaType> list = new ArrayList<MediaType>();
-	
-		list.add(MediaType.ALL);
-		//Change this based on GCP project : <PROJECT_ID>
-		headers.add("x-goog-user-project", "<PROJECT_ID>");
-		headers.setAccept(list);
-		//Bearer Token
-		//to get temporary GCP token run this command from Google cloud shell "gcloud auth print-access-token"
-		//this can also be retrieved through proper Google cloud api and store it in cache as per your requirement
-		headers.setBearerAuth("<<GCP_BEARER_TOKEN>>");
-		//Request object creation
-		CreateIntentRequest createIntentRequest = new CreateIntentRequest();
-		List<TrainingPhrases> tPhrases = new ArrayList<>();
-		
-		//Ensuring the Bullets and numberings are removed from the OpenAI response and add newline feed.
-		for(int i=0;i<numOfPhrase; i++) {
-			int j =0;
-			if(trainingPhrases[i].startsWith("1") || trainingPhrases[i].startsWith("2") || trainingPhrases[i].startsWith("3") || 
-					trainingPhrases[i].startsWith("4") || trainingPhrases[i].startsWith("5") || trainingPhrases[i].startsWith("6") 
-					|| trainingPhrases[i].startsWith("7") || trainingPhrases[i].startsWith("8") ||
-					trainingPhrases[i].startsWith("9") || trainingPhrases[i].startsWith("10")) {
-					entryText = trainingPhrases[i].substring(3);
-					System.out.println("text " + entryText);
-			}else {
-				entryText = trainingPhrases[i];
-				}
-			System.out.println("entry Text " + entryText);
-			
-			//validation and forming request object in the required format
-			/*
-			 * { "displayName": "TravelDispName", "trainingPhrases": [ { "parts": [ {
-			 * "parameterId": "", "text": "I need to plan my trip" } ], "repeatCount": 1 },
-			 * { "parts": [ { "parameterId": "", "text":
-			 * "Help me to build itinerary for the upcoming trip" } ], "repeatCount": 1 } ]
-			 * }
-			 */
-			if(entryText !=null && !entryText.isBlank() && !entryText.isEmpty()) {
-				List<Parts> partsList1 = new ArrayList<>();
-				Parts firstText = new Parts();
-				firstText.setParameterId("");
-				firstText.setText(entryText);
-				partsList1.add(0, firstText);
-				
-				TrainingPhrases trPh1 = new TrainingPhrases();
-				trPh1.setParts(partsList1);
-				trPh1.setRepeatCount("1");
-				
-				tPhrases.add(j, trPh1);
-				j++;
-				}
-			else {
-
-				List<Parts> partsList1 = new ArrayList<>();
-				Parts firstText = new Parts();
-				firstText.setParameterId("");
-				firstText.setText(entryText);
-				partsList1.add(0, firstText);
-				
-				TrainingPhrases trPh1 = new TrainingPhrases();
-				trPh1.setParts(partsList1);
-				trPh1.setRepeatCount("1");
-				
-				tPhrases.add(j, trPh1);
-				j++;
-				
-			}
-		}
-		
-		createIntentRequest.setDisplayName(intentName);
-		createIntentRequest.setTrainingPhrases(tPhrases);
-		
-		System.out.println(createIntentRequest);
-		
-		HttpEntity<CreateIntentRequest> waRequest = new HttpEntity<CreateIntentRequest>(createIntentRequest, headers);
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.exchange(url, HttpMethod.POST, waRequest, DFCXApiResponse.class);
-	
-	}
-	
-	
 	
 }
